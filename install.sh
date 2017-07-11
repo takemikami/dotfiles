914905e678c17cf3f47d820a90373dd3c2a2790f
Takeshi Mikami <takeshi.mikami@gmail.com>
2017-07-11 19:15:13 +0900

    update install script

--- install.sh
+++ install.sh
@@ -5,6 +5,23 @@ cd $(dirname $0)
 for f in `find home`; do
   tgt=${f:5}
   if [ ${#tgt} -gt 0 ]; then
-    echo "cp home/${tgt} ~/${tgt}"
+    if [ ! -f ~/${tgt} ]; then
+      # new file
+      cp home/${tgt} ~/${tgt}
+      echo "home/${tgt} -> ~/${tgt} (new file)"
+    else
+      # check difference
+      linecnt=$(diff home/${tgt} ~/${tgt} | wc -l)
+      if [ $linecnt -ne 0 ]; then
+        diff home/${tgt} ~/${tgt}
+        echo "update ~/${tgt}, ok(y/N)?"
+        read INPUT
+        if [ "$INPUT" = "y" ]; then
+          cp ~/${tgt} ~/${tgt}.bak
+          cp home/${tgt} ~/${tgt}
+          echo "home/${tgt} -> ~/${tgt} (update)"
+        fi
+      fi
+    fi
   fi
 done
